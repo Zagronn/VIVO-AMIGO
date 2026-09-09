2de29e2597812592ed84bbc5c63ace229364ab31
SERDAR CEVIK <serdarcevik@SERDARs-MacBook-Pro.local>
2026-09-08 22:10:42 -0600

    feat: complete VIVO AMIGO brand design and UI polish

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {__rewrites:{afterFiles:[{has:a,source:"\u002Fmarketplace\u002F:path*",destination:a},{has:a,source:"\u002Fwallet\u002F:path*",destination:a},{has:a,source:"\u002Flogistics\u002F:path*",destination:a}],beforeFiles:[],fallback:[]},sortedPages:["\u002F_app"]}}(void 0));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
